a818b1cf4d68396e47b279898e0d1dbce816b0b4
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-21 12:42:39 +0600

    load user data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [userData, setUserData] = useState([]);
+  const [details, setDetails] = useState([]);
+
+  const getUsers = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get(
+        "https://602e7c2c4410730017c50b9d.mockapi.io/users"
+      );
+      setUserData(data);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+    }
+  };
+
+  useEffect( () => {
+    getUsers();
+  }, [])
+
   return (
     <div className="App">
-      hello world
+      {userData.map(user => <h3 key={user.id}>{user.id}</h3>)}
     </div>
   );
 }
